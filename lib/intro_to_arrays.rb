7b27e1c00c4d7571303ad615d1641e01b2522141
Marco Miranda <marmirand@hotmail.com>
2019-03-31 19:48:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,3 +15,8 @@ def third_element(array)
   array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   array [2]
 end
+
+def third_element(array)
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array [-1]
+end
