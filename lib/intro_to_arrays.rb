17191022943460cebedec522401cdc690273984f
Marco Miranda <marmirand@hotmail.com>
2019-03-31 19:51:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -30,3 +30,7 @@ def last_element_with_array_methods(array)
   array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   array.last
 end
+
+def length_of_array(array)
+  array = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  array.length
